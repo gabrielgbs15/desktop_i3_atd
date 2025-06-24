d5a4820178655334496bcd0cab7d6d49553ad1be
gabrielgbs15 <109381651+gabrielgbs15@users.noreply.github.com>
2025-06-24 18:17:14 -0300

    Delete Desktop_i3_atzd directory

--- Desktop_i3_atzd/i3blocks/scripts/keyindicator.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-symbol="󰪛"  # Troque por outro símbolo se quiser
-
-# Checa o estado do Caps Lock
-if xset q | grep 'Caps Lock:' | grep -q 'on'; then
-    echo "$symbol"
-fi
--- Desktop_i3_atzd/i3blocks/scripts/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-updates=$(checkupdates 2>/dev/null | wc -l)
-
-if [ "$updates" -eq 0 ]; then
-    echo ""
-else
-    echo " $updates"
-fi
